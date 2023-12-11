848f07bfd2ec8835cb50ac442900e2dec5db8522
Fred Jabbari <fjabbari@emoneyadvisor.com>
2023-12-11 12:41:51 -0500

    ainitial work2

--- configService/configService/Controllers/WeatherForecastController.cs
+++ configService/configService/Controllers/WeatherForecastController.cs
@@ -25,7 +25,7 @@ namespace configService.Controllers
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = "V0.0:Author:Fredj:Detail:" + Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
         }
